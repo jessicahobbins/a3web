5f683271faa20f9df76a04fe0536bb8937a40df1
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2020-12-07 21:01:17 -0500

    Updating javascript variable names

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -22,15 +22,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");           // Calling mySlides element, which contains the images
-  var dots = document.getElementsByClassName("opacity");              // Calling the opacity element, which reduces the opacity of the thumbnail not currently selected
+  var thumbnails = document.getElementsByClassName("opacity");        // Calling the opacity element, which reduces the opacity of the thumbnail not currently selected
   if (n > slides.length) {slideIndex = 1}                             // Displaying slide 1 as default
   if (n < 1) {slideIndex = slides.length}                             // slideIndex is equal to the length of the slides that have been coded in the HTML
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";                                 // Hiding the slides that are not currently selected
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");     // Displaying the slides that are currently selected
+  for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "");     // Displaying the slides that are currently selected
   }
-  slides[slideIndex-1].style.display = "block";                      // Displaying as block level element
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";                       // Displaying as block level element
+  thumbnails[slideIndex-1].className += " active";
 }
